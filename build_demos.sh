9fe6916d5643ff9b3e4d5d721e2f5d3f04bd6b87
Noe Nieto <nnieto@noenieto.com>
2023-11-10 01:42:39 +0000

    Script to build demos/

---
+++ build_demos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building LM317 calculator ..."
+git clone git@github.com:misaelnieto/lm317-calculator.git _lm317
+cd _lm317
+yarn install
+yarn build --if-present
+mkdir -p ../demos/lm317-calculator/
+mv build/* ../demos/lm317-calculator/
+cd ..
+echo "Building LM317 calculator ... DONE"
